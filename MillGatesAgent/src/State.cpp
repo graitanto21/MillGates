a1fe35223dec3a37f535f73330537aadfaa0c765
luca_bonfiglioli <luca.bonfiglioli@gmail.com>
2018-04-21 10:05:31 +0200

    Created empty project to test if everything is fine

---
+++ MillGatesAgent/src/State.cpp
@@ -0,0 +1,18 @@
+/*
+ * State.cpp
+ *
+ *  Created on: Apr 21, 2018
+ *      Author: Luca
+ */
+
+#include "State.h"
+
+State::State() {
+	// TODO Auto-generated constructor stub
+
+}
+
+State::~State() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ MillGatesAgent/src/State.h
@@ -0,0 +1,17 @@
+/*
+ * State.h
+ *
+ *  Created on: Apr 21, 2018
+ *      Author: Luca
+ */
+
+#ifndef STATE_H_
+#define STATE_H_
+
+class State {
+public:
+	State();
+	virtual ~State();
+};
+
+#endif /* STATE_H_ */
